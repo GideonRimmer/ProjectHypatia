e8a890e2509df9489096404abed912a87a3af2eb
Janek <jkjaniszewski@gmail.com>
2020-05-12 15:38:53 +0200

    CharcterController

---
+++ Hypatia/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float sensitivity = 1f;
+    public Transform PlayerBody;
+    float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * 100 * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * 100 * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        PlayerBody.Rotate(Vector3.up * mouseX);
+    }
+}
