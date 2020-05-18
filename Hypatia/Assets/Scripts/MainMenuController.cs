533a3de8d41792445f50d69461c425975e3d338a
GideonRimmer <55274618+GideonRimmer@users.noreply.github.com>
2020-05-18 12:47:56 +0200

    Menu navigation scripts.

---
+++ Hypatia/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public String gameSceneName;
+    public String creditsSceneName;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void OpenCreditsMenu()
+    {
+        SceneManager.LoadScene(creditsSceneName);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        Application.Quit();
+    }
+}
