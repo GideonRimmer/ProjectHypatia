20145c24f98a7c1cdc35b2f6681caf34a9d2e3ca
GideonRimmer <55274618+GideonRimmer@users.noreply.github.com>
2020-05-21 01:34:04 +0200

    Key effect particle system and implement in prefabs.

--- Hypatia/Assets/Scripts/KeyController.cs
+++ Hypatia/Assets/Scripts/KeyController.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
+    public ParticleSystem keyEffect;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
             Debug.Log("Key picked up");
+            Instantiate(keyEffect, this.transform.position, Quaternion.identity);
+
             Destroy(this.gameObject);
         }
     }
