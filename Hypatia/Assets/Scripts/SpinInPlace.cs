b095eb21c7f32a23585d4c7334b84a22aa25f966
GideonRimmer <55274618+GideonRimmer@users.noreply.github.com>
2020-05-20 17:45:57 +0200

    Key prefab and scripts.

---
+++ Hypatia/Assets/Scripts/SpinInPlace.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinInPlace : MonoBehaviour
+{
+    public float rotateSpeed = 5.0f;
+
+    void Update()
+    {
+        transform.Rotate(0, rotateSpeed, 0);
+    }
+}
