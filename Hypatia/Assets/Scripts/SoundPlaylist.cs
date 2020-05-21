8ee599b9405f61356cf91df83b5f5f24571e7cc4
GideonRimmer <55274618+GideonRimmer@users.noreply.github.com>
2020-05-21 15:57:37 +0200

    Random sound player script and prefabs.

---
+++ Hypatia/Assets/Scripts/SoundPlaylist.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlaylist : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] audioClips;
+
+    void Start()
+    {
+        audioSource.GetComponent<AudioSource>();
+        audioSource.loop = false;
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.clip = GetRandomClip();
+            audioSource.Play();
+        }
+    }
+
+    private AudioClip GetRandomClip()
+    {
+        return audioClips[Random.Range(0, audioClips.Length)];
+    }
+}
