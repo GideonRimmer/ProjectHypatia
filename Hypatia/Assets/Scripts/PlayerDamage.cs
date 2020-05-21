62091955bf065efdac807d8233e9a3c6b4ff06b2
GideonRimmer <55274618+GideonRimmer@users.noreply.github.com>
2020-05-21 17:52:27 +0200

    Update PlayerDamage.cs

--- Hypatia/Assets/Scripts/PlayerDamage.cs
+++ Hypatia/Assets/Scripts/PlayerDamage.cs
@@ -6,34 +6,35 @@ public class PlayerDamage : MonoBehaviour
 {
     public int maxHitPoints = 5;
     [SerializeField] private int currentHitPoints;
+    public float pushForce = 10.0f;
+
+    private Rigidbody rigidbody;
+    private CharacterController characterController;
     
-    //private Rigidbody rigidbody;
-    //public float pushForce = 10.0f;
     void Start()
     {
-        //rigidbody = GetComponent<Rigidbody>();
-        currentHitPoints = maxHitPoints;
-    }
+        rigidbody = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (currentHitPoints <= 0)
-        {
-            Debug.Log("GAME OVER!");
-            // TODO: GameOver();
-        }
+        currentHitPoints = maxHitPoints;
     }
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Enemy" && currentHitPoints >= 0)
+        //if (other.gameObject.tag == "Enemy" && currentHitPoints >= 0)
+        if (other.gameObject.tag == "Enemy")
         {
             // Reduce the player's hit points.
             currentHitPoints -= 1;
             Debug.Log("Player hit! Current HP: " + currentHitPoints);
 
             // TODO: play hit effect
+
+            if (currentHitPoints <= 0)
+            {
+                Debug.Log("GAME OVER!");
+                // TODO: GameOver();
+            }
 
             /*
             // Push the player back.
